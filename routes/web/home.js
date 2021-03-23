b7d013e9452f803c5afce904c606640ad258f0ed
Randy Chan <charan@dunwoody.edu>
2021-03-23 13:47:04 -0500

    Minor changes.

--- routes/web/home.js
+++ routes/web/home.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
 //Index route
 router.get("/", function(req, res){
     
     console.log("Index");
-    res.render("home/");
+    res.render("home/home");
 });
-
-// Home route.
 router.get("/home", function(req, res){
     console.log("Home");
     res.render("home/home");
 });
-
 router.get("/about", function(req, res){
     console.log("About");
     res.render("home/about");
