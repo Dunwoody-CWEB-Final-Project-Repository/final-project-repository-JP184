c54f8b12be4b94c283e47d5e2b6a05d442bf7e06
Randy Chan <charan@dunwoody.edu>
2021-04-01 19:47:09 -0500

    Completed Homepage, completed Whoareyou

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs"); // Sets viewengine to use EJS.
 
 app.use("/", require("./routes/web")); // Access web routes.
 app.use("/api", require("./routes/api")); // Access api routes.
-app.use(express.static(__dirname + "/public")); // To be able to access any files in our public folder.
+app.use(express.static(path.join(__dirname + "/public"))) // To be able to access any files in our public folder.
 
 app.listen(app.get('port'), function(){
     console.log("Server started on port " + app.get("port"));
