a86517552249cadc6e8fe74ef37dc001eaebe03d
Randy Chan <charan@dunwoody.edu>
2021-04-16 13:25:39 -0500

    DB testing.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { Client } = require('pg');
 
-router.get('/user', (req, res) => {
-  res.json('This is a json status code for the users api');
+router.get('/user', function (req, res) {
+  res.json('This is a json status code.');
 })
 
 module.exports = router;
