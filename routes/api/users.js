856984be31a2347555d0cf0c2df3b11c4d6367a1
Randy Chan <charan@dunwoody.edu>
2021-04-16 13:43:46 -0500

    DB Testing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 router.get('/', function (req, res) {
-  res.json('This is a json status code.');
+  try{
+    const client = await pool.connect()
+    const query = await client.query('SELECT * FROM test_table');
+    const result = {'result': (query) ? result.rows : null}
+    res.render(JSON.stringify(result))
+    client.release()
+  } catch (err) {
+    console.error(err)
+    res.send("Error " + err)
+  }
 })
 
 module.exports = router;
